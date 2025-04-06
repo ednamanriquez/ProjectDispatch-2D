54b59f31cd12ed9a74dcfe3c96565989d3858894
Edna <112024942+ednamanriquez@users.noreply.github.com>
2025-04-06 18:37:50 -0500

    script

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,22 @@
+using System.IO;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private string saveLocation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData");
     }
 
     // Update is called once per frame
-    void Update()
+    public void SaveGame()
     {
-        
+        SaveData saveData = new SaveData
+        {
+            playerPos = GameObject.Find("Player").transform.position,
+            mapBoundary = GameObject.Find()
+        }
     }
 }
--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
+[System.Serializable]
 public class SaveData
-{
-    
+{   
+    public Vector2 playerPos;
+    public string mapBoundary;
+
 }
